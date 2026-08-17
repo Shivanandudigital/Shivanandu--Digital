fd75511b3d052d07ad6644df1797c1cdb128869c
Shivanandudigital <shivanandudigital2020@gmail.com>
2026-08-17 20:44:44 +0530

    Revert "Expand Independence Day flag across page"
    This reverts commit d2d947c7457e7dde50516aeb1f81f4ffd1f74ea6.

--- components/IndependenceDayWatermark.tsx
+++ components/IndependenceDayWatermark.tsx
@@ -1,21 +1,17 @@
 export default function IndependenceDayWatermark() {
   return (
-    <>
-      <div className="independence-page-flag" aria-hidden="true">
-        <span className="independence-page-flag__band independence-page-flag__band--saffron" />
-        <span className="independence-page-flag__band independence-page-flag__band--white">
-          <span className="independence-page-flag__chakra">✺</span>
+    <div className="independence-watermark" aria-hidden="true">
+      <div className="independence-watermark__wish">
+        <span>🇮🇳 Happy Independence Day 🇮🇳</span>
+        <strong>স্বাধীনতা দিবসের আন্তরিক শুভেচ্ছা • জয় হিন্দ</strong>
+      </div>
+      <div className="independence-watermark__flag">
+        <span className="independence-watermark__stripe independence-watermark__stripe--saffron" />
+        <span className="independence-watermark__stripe independence-watermark__stripe--white">
+          <span className="independence-watermark__chakra">✺</span>
         </span>
-        <span className="independence-page-flag__band independence-page-flag__band--green" />
+        <span className="independence-watermark__stripe independence-watermark__stripe--green" />
       </div>
-
-      <div className="independence-wish" aria-label="Independence Day wishes">
-        <span className="independence-wish__english">🇮🇳 Happy Independence Day</span>
-        <strong className="independence-wish__bengali">
-          স্বাধীনতা দিবসের আন্তরিক শুভেচ্ছা
-        </strong>
-        <span className="independence-wish__jai-hind">জয় হিন্দ 🇮🇳</span>
-      </div>
-    </>
+    </div>
   );
 }
